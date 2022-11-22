02b8eb8849dbce5ca82eff7273b26570d9e713f1
asapconet <aaronrickymeek@gmail.com>
2022-11-22 03:19:29 +0100

    chore: major code refactor with ts

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const App = () => {
-  return (
-    <div>The university of jos portal</div>
-  )
-}
-
-export default App
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SidebarWithHeader from './components/HomeLayout'
+function App() {
+  return (
+    <>
+      <SidebarWithHeader>Th9is this this </SidebarWithHeader>
+    </>
+  );
+}
+
+export default App;
